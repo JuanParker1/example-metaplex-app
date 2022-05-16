bea43016e2d8f458c9647de248072cd54386a045
Henry Hobhouse <henryhobhouse@bulb.co.uk>
2022-05-16 07:46:41 +0100

    feat: setup firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDfn-rhZxZB4SfAVVKtd87m7hm0UgpAMrU',
+  authDomain: 'example-metaplex.firebaseapp.com',
+  projectId: 'example-metaplex',
+  storageBucket: 'example-metaplex.appspot.com',
+  messagingSenderId: '381161497660',
+  appId: '1:381161497660:web:e46f040edf97f71e788f39',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
